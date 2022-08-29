b548b772c00cc7a9e4d320e4bf7f27a8ab015a38
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-29 15:02:38 -0500

    Fix linter errors

--- app/models/Food.rb
+++
@@ -1,9 +0,0 @@
-class Food < ApplicationRecord
-    validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
-    validates :measurement_unit, presence: { allow_blank: false, message: 'must not be blank.' }
-    validates :price, comparison: { greater_than_or_equal_to: 0}
-    validates :quantity, comparison: { greater_than: 0}
-
-    has_many :recipes, through: :recipe_foods
-    belongs_to :user
-end
--- app/models/User.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
-
-    has_many :recipes
-    has_many :foods
-end
