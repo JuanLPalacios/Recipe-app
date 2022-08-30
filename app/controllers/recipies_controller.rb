167e30bbebdaa1c82e04692eed55f59a95956924
JuanLP <juan.luis.palacios.p@gmail.com>
2022-08-30 14:27:37 -0500

    Fix linter errors

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,9 +1,7 @@
 class RecipiesController < ApplicationController
-  def show
-  end
+  def show; end
 
-  def index
-  end
+  def index; end
 
   def public_recipes
     @recipes = Recipe.where(public: true).order(created_at: :desc)
--- test/controllers/recipies_controller_test.rb
+++ test/controllers/recipies_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get recipies_index_url
     assert_response :success
   end
 
-  test "should get public_recipes" do
+  test 'should get public_recipes' do
     get recipies_public_recipes_url
     assert_response :success
   end
