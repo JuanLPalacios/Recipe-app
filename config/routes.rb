8b4e870d128859e03c7620775c685edd77e1fb8c
Trictonicmp <code_shadow@hotmail.com>
2022-08-30 13:41:57 -0500

    Fix route for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'foods/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :foods
   end
+  resources :foods
   # Defines the root path route ("/")
   # root "articles#index"
 end
