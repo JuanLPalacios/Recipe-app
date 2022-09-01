b91eea0bab5ec37ce59264b54c8d4dedf43d54b6
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-01 14:22:03 -0500

    Add new recipes view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,7 +17,25 @@ class RecipesController < ApplicationController
 
   def public_recipes
     @recipes = Recipe
-      .includes(:user, :foods)
+      .includes(:user, :recipes)
       .where(public: true).order(created_at: :desc)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparetion_time, :cooking_time, :description, :public)
   end
 end
